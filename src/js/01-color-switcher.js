74f4ba6af0fdef8c7ff3f955cedd92628b1bd5cd
Bilous Sviatoslav <sviat24@gmail.com>
2022-12-19 22:46:28 +0200

    Home work #9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+class Timer {
+  constructor(onTick, delay) {
+    this.interval = null;
+    this.onTick = onTick;
+    this.delay = delay;
+  }
+
+  startTimer() {
+    if (!this.interval) {
+      this.interval = setInterval(this.onTick, this.delay);
+    }
+  }
+
+  stopTimer() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+}
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+
+const setBodyRandColor = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+let timer = new Timer(setBodyRandColor, 1000);
+
+refs.start.addEventListener('click', timer.startTimer.bind(timer));
+refs.stop.addEventListener('click', timer.stopTimer.bind(timer));
---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+const refs = {
+  timerBtn: document.querySelector('[data-start]'),
+  timerDays: document.querySelector('[data-days]'),
+  timerHours: document.querySelector('[data-hours]'),
+  timerMinutes: document.querySelector('[data-minutes]'),
+  timerSeconds: document.querySelector('[data-seconds]'),
+};
+
+let timerId = null;
+let selectedDate = null;
+let currentDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= options.defaultDate) {
+      Notify.failure('Please chose a date in the future');
+    } else {
+      refs.timerBtn.disabled = false;
+    }
+
+    selectedDate = selectedDates[0].getTime();
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+refs.timerBtn.disabled = true;
+
+refs.timerBtn.addEventListener('click', onStartTimer);
+
+function onStartTimer() {
+  const startDate = selectedDate;
+
+  timerId = setInterval(() => {
+    refs.timerBtn.disabled = true;
+
+    currentDate = Date.now();
+    const deltaTime = startDate - currentDate;
+    const time = convertMs(deltaTime);
+
+    updateTimerValues(time);
+    stopInterval(deltaTime);
+  }, 1000);
+}
+
+function stopInterval(deltaTime) {
+  if (deltaTime < 1000) {
+    clearInterval(timerId);
+  }
+}
+
+function updateTimerValues({ days, hours, minutes, seconds }) {
+  refs.timerDays.textContent = `${days}`;
+  refs.timerHours.textContent = `${hours}`;
+  refs.timerMinutes.textContent = `${minutes}`;
+  refs.timerSeconds.textContent = `${seconds}`;
+}
